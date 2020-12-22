7155f95e7780f80f7efb44ef2636ab568423b3b0
aurcode <aurelio3927@gmail.com>
2020-12-22 17:27:31 -0400

    getUsers

--- controllers/index.controllers.ts
+++ controllers/index.controllers.ts
@@ -1,4 +1,39 @@
-export const getBooks = () => {}
+import { Response } from "https://deno.land/x/oak/mod.ts";
+
+interface Book {
+    id: string,
+    name: string,
+    author: string
+    originalLanguage: string
+}
+
+const books: Book[] = [
+    {
+        id: "1",
+        name: "Clean code: Style guide for agile development",
+        author: "Robert C. Martin book",
+        originalLanguage: "English"
+    },
+    {
+        id: "2",
+        name: "Eloquent JavaScript: A Modern Introduction to Programming",
+        author: "Marijn Haverbeke",
+        originalLanguage: "English"
+    },
+    {
+        id: "3",
+        name: "Design Patterns",
+        author: "Erich Gamma, John Vlissides, Richard Helm, Ralph Johnson",
+        originalLanguage: "English"
+    },
+];
+
+export const getBooks = ({response}: {response: Response}) => {
+    response.body = {
+        message: 'successful Query',
+        books
+    }
+}
 export const getBook = () => {}
 export const createBook = () => {}
 export const updateBook = () => {}
